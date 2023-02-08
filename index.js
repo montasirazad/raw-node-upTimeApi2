26b0420a34d6d29567df72915bf3badd3f0af6a0
unknown <montasirazad@gmail.com>
2023-02-09 02:44:18 +0600

    completed part 7

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environments = require('./helpers/environments');
-const data = require('./lib/data')
-const { sendTwilioSms } = require('./helpers/notification')
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
 const app = {};
 
-app.config = {
-    port: 5000
-}
-
-
-
-// data.update('test', 'new', { name: 'AAAA', age: 22,job:'coding' }, (err) => {
-//     console.log(err);
-// })
-
-
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${environments.port}`);
-    })
+app.init = () => {
+    
+    server.init();
+    workers.init();
 };
 
-app.handleReqRes = handleReqRes;
+app.init();
 
-app.createServer();
+module.exports = app;
+
+
+
+
